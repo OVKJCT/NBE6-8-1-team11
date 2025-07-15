96b1c37d2a721a56bd47e3abd6b3da5f02bac3c4
JIWONKIMS <107270746+JIWONKIMS@users.noreply.github.com>
2025-07-16 00:27:12 +0900

    add coffee cotroller (#31)

--- backend/src/main/java/com/back/domain/coffee/dto/CoffeeDto.java
+++ backend/src/main/java/com/back/domain/coffee/dto/CoffeeDto.java
@@ -1,7 +1,12 @@
 package com.back.domain.coffee.dto;
 
+import com.back.domain.coffee.entity.Coffee;
+
 public record CoffeeDto(
         String coffeeName,
         int coffeePrice
 ) {
+    public CoffeeDto(Coffee coffee) {
+        this(coffee.getCoffeeName(), coffee.getCoffeePrice());
+    }
 }
