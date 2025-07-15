50f373722d33a04322227965b807e3f8045f49af
JIWONKIMS <107270746+JIWONKIMS@users.noreply.github.com>
2025-07-15 17:50:41 +0900

    add data (#28)

--- backend/src/main/java/com/back/global/initData/BaseInitData.java
+++ backend/src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.global.initData;
 
+import com.back.domain.coffee.service.CoffeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -8,12 +9,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.back.domain.coffee.entity.CoffeeNames.*;
+
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
     @Autowired
     @Lazy
     private BaseInitData self;
+
+    @Autowired
+    private CoffeeService coffeeService;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
@@ -25,5 +31,11 @@ public class BaseInitData {
     @Transactional
     public void work1() {
         //초기화
+        if(coffeeService.count() == 0) {
+            coffeeService.addCoffee(COLUMBIA_NARINO, 2000);
+            coffeeService.addCoffee(COLUMBIA_QUINDIO, 2500);
+            coffeeService.addCoffee(ETHIOPIA, 3000);
+            coffeeService.addCoffee(BRAZILL, 3500);
+        }
     }
 }
