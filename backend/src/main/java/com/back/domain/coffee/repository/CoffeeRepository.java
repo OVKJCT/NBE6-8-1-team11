624b002491910e6f3adf3130feafa4c8e9e5e69e
JIWONKIMS <giwon11292@gmail.com>
2025-07-15 17:45:47 +0900

    add data

--- backend/src/main/java/com/back/domain/coffee/repository/CoffeeRepository.java
+++ backend/src/main/java/com/back/domain/coffee/repository/CoffeeRepository.java
@@ -3,6 +3,8 @@ package com.back.domain.coffee.repository;
 import com.back.domain.coffee.entity.Coffee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CoffeeRepository extends JpaRepository<Coffee, Integer> {
+import java.util.Optional;
 
+public interface CoffeeRepository extends JpaRepository<Coffee, Integer> {
+    Optional<Coffee> findByCoffeeName(String coffeeName);
 }
