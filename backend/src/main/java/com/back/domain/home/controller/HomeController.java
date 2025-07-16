c1c7450f8ce1a120143d3789d8719c7c1835d651
EoN <jaegyun1011@gmail.com>
2025-07-16 10:56:40 +0900

    API 문서 작성 (#36)

--- backend/src/main/java/com/back/domain/home/controller/HomeController.java
+++ backend/src/main/java/com/back/domain/home/controller/HomeController.java
@@ -1,31 +1,32 @@
 package com.back.domain.home.controller;
 
-import com.back.domain.coffee.entity.CoffeeNames;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+
+import static java.net.InetAddress.getLocalHost;
+import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
+
 @RestController
+@Tag(name = "HomeController", description = "홈 컨트롤러")
 public class HomeController {
-    @GetMapping("/")
+    @SneakyThrows
+    @GetMapping(produces = TEXT_HTML_VALUE)
+    @Operation(summary = "메인 페이지")
     public String main() {
+        InetAddress localHost = getLocalHost();
+
         return """
                 <h1>API 서버</h1>
-                <p>홈 페이지입니다.</p>
+                <p>Host Name: %s</p>
+                <p>Host Address: %s</p>
                 <div>
-                    <h2>API 문서</h2>
-                    <h3>커피 종류:</h3>
-                    <ul>
-                        <li>%s</li>
-                        <li>%s</li>
-                        <li>%s</li>
-                        <li>%s</li>
-                    </ul>
+                    <a href="/swagger-ui/index.html">API 문서로 이동</a>
                 </div>
-                """.formatted(
-                CoffeeNames.COLUMBIA_NARINO,
-                CoffeeNames.BRAZILL,
-                CoffeeNames.COLUMBIA_QUINDIO,
-                CoffeeNames.ETHIOPIA
-        );
+                """.formatted(localHost.getHostName(), localHost.getHostAddress());
     }
 }
--- backend/src/main/java/com/back/domain/order/controller/OrderController.java
+++ backend/src/main/java/com/back/domain/order/controller/OrderController.java
@@ -3,7 +3,10 @@ package com.back.domain.order.controller;
 import com.back.domain.order.dto.OrderDto;
 import com.back.domain.order.entity.CoffeeOrder;
 import com.back.domain.order.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/")
 @RequiredArgsConstructor
+@Tag(name = "OrderController", description = "API 주문 컨트롤러")
 public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/orders")
+    @Transactional(readOnly = true)
+    @Operation(summary = "주문 목록 조회")
     public OrderDto createOrder(@Validated @ModelAttribute("order") CoffeeOrder order) {
         CoffeeOrder save = orderService.addOrder(order);
 
