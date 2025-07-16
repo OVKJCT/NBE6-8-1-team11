7336e883e0fc033e32341abaa8e1c96a2aae5289
EoN <jaegyun1011@gmail.com>
2025-07-16 15:02:21 +0900

    이미지 API (#42)

--- backend/src/main/java/com/back/domain/coffee/controller/CoffeeController.java
+++ backend/src/main/java/com/back/domain/coffee/controller/CoffeeController.java
@@ -6,11 +6,19 @@ import com.back.domain.coffee.service.CoffeeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -31,5 +39,19 @@ public class CoffeeController {
                 .stream()
                 .map(CoffeeDto::new)
                 .toList();
+    }
+
+    @GetMapping("/coffees/{id}")
+    @Transactional(readOnly = true)
+    @Operation(summary = "커피 이미지 조회")
+    public ResponseEntity<byte[]> getCoffeeImage(@PathVariable int id) throws IOException {
+        ClassPathResource imgFile = new ClassPathResource("static/images/" + id + ".png");
+
+        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+
+        return new ResponseEntity<>(bytes, headers, HttpStatus.OK);
     }
 }
