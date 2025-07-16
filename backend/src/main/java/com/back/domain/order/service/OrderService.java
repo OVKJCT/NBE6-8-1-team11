d50b3e8b12ec980fff63da9a7c3cd3940c88afd2
jaegyun1011 <jaegyun1011@naver.com>
2025-07-16 16:41:59 +0900

    주문 처리 스케줄러 추가

--- backend/src/main/java/com/back/domain/order/service/OrderService.java
+++ backend/src/main/java/com/back/domain/order/service/OrderService.java
@@ -3,8 +3,12 @@ package com.back.domain.order.service;
 import com.back.domain.order.entity.Order;
 import com.back.domain.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional
@@ -21,4 +25,15 @@ public class OrderService {
     }
 
     //주문 조회 메서드 추가 가능
+
+    //매일 오루 2시에 주문 처리
+    @Scheduled(cron = "0 0 14 * * *")
+    public void processOrders() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Order> orders = orderRepository.findByCreateDateBefore(now);
+
+        for(Order o : orders) {
+            //Order 삭제 코드
+        }
+    }
 }
