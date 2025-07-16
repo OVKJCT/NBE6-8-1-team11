6db84de4ed621c0c57be60c90e5b2547c56d2c8b
GarakChoi <tpghks312@naver.com>
2025-07-16 12:36:31 +0900

    add id

--- backend/src/main/java/com/back/domain/coffee/dto/CoffeeDto.java
+++ backend/src/main/java/com/back/domain/coffee/dto/CoffeeDto.java
@@ -3,10 +3,11 @@ package com.back.domain.coffee.dto;
 import com.back.domain.coffee.entity.Coffee;
 
 public record CoffeeDto(
+        int id,
         String coffeeName,
         int coffeePrice
 ) {
     public CoffeeDto(Coffee coffee) {
-        this(coffee.getCoffeeName(), coffee.getCoffeePrice());
+        this(coffee.getId(), coffee.getCoffeeName(), coffee.getCoffeePrice());
     }
 }
